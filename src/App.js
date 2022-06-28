5dcce66848bb7a8f906a9f9ef319058b452058ed
Paige W <ward.paige96@gmail.com>
2022-06-28 17:53:48 -0600

    card images, pseudocode for shuffle feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+// array of each card's front face image to later shuffle through
+const cardFronts = [
+  {'src' : './images/brick.jpeg'},
+  {'src' : './images/oar.jpeg'},
+  {'src' : './images/sheep.jpeg'},
+  {'src' : './images/wheat.jpeg'},
+  {'src' : './images/wood.jpeg'}
+]
+
 function App() {
+
+  // SHUFFLING
+  // grab the card images array and sort through
+  // randomly select a number 
+  // each numer should equate to the id of the images in the array
+  // set the state for shuffling
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory: A Catan Twist</h1>
+      <section className='cardLayout'>
+        {cards.map(card => (
+          <div className='card' key={card.id}>
+            <img className='front' src={card.src} alt='front of card' />
+            <img className='back' src='./images/back.jpeg' alt='back of card' />
+          </div>
+        ))}
+        
+        
+      </section>
     </div>
   );
 }
