2c98d844204d78135113b3c19203be32283640c7
Paige W <ward.paige96@gmail.com>
2022-06-29 17:53:47 -0600

    only show back of cards until clicked

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import { useState } from 'react';
-import './App.css';
-import Card from './components/Card';
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   // Array of each card's front face image
   const cardFronts = [
-    {'src' : '/images/brick.jpeg'},
-    {'src' : '/images/brick.jpeg'},
-    {'src' : '/images/oar.jpeg'},
-    {'src' : '/images/oar.jpeg'},
-    {'src' : '/images/sheep.jpeg'},
-    {'src' : '/images/sheep.jpeg'},
-    {'src' : '/images/wheat.jpeg'},
-    {'src' : '/images/wheat.jpeg'},
-    {'src' : '/images/wood.jpeg'},
-    {'src' : '/images/wood.jpeg'}
-  ]
+    { src: "/images/brick.jpeg" },
+    { src: "/images/brick.jpeg" },
+    { src: "/images/oar.jpeg" },
+    { src: "/images/oar.jpeg" },
+    { src: "/images/sheep.jpeg" },
+    { src: "/images/sheep.jpeg" },
+    { src: "/images/wheat.jpeg" },
+    { src: "/images/wheat.jpeg" },
+    { src: "/images/wood.jpeg" },
+    { src: "/images/wood.jpeg" },
+  ];
 
-  const [cards, setCards] = useState([])
-  const [points, setPoints] = useState(0)
+  const [cards, setCards] = useState([]);
+  const [points, setPoints] = useState(0);
+  const [firstClick, setFirstClick] = useState(null);
+  const [secondClick, setSecondClick] = useState(null);
 
   const shuffle = () => {
     const shuffled = [...cardFronts]
-    .sort(() => Math.random() - 0.5)
-    .map((each) => ({...each, id: Math.random()}))
+      .sort(() => Math.random() - 0.5)
+      .map((each) => ({ ...each, id: Math.random() }));
 
-    setCards(shuffled)
-    setPoints(0)
-  }
-
+    setCards(shuffled);
+    setPoints(0);
+  };
   // // Fisher-Yates Shuffle
   // const shuffle = (cardFronts) => {
   //   const shuffledCards = (cardFronts) => {
@@ -43,19 +44,26 @@ function App() {
   //   }
   //   setCards(shuffledCards())
   //   setPoints(0)
-  // } 
+  // }
+
+  const handleCard = (card) => {
+    if (firstClick === true) {
+      setFirstClick(card)
+    } 
+    else {
+      setSecondClick(card)
+    }
+  }
 
   return (
     <div className="App">
       <h1>Memory: A Catan Twist</h1>
       <h2>Points: {points}</h2>
       <button onClick={shuffle}>Shuffle</button>
-      <section className='cardLayout'>
-        {cards.map(card => (
-         <Card key={card.id} card={card}/>
+      <section className="cardLayout">
+        {cards.map((card) => (
+          <Card key={card.id} card={card} handleCard={handleCard} flip={card === firstClick} />
         ))}
-        
-        {/* {cards} */}
       </section>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,16 @@
-const Card = ({card}) => {
+import './Card.css'
+
+const Card = ({card, handleCard, flip}) => {
+    const handleClick = () => {
+        handleCard(card)
+    }
+
     return (
         <div className='card'>
-            <img className='front' src={card.src} alt='front of card' />
-            <img className='back' src='./images/back.jpeg' alt='back of card' />
+            <div className={flip ? 'flip' : ''}>
+                <img className='front' src={card.src} alt='front of card' />
+                <img className='back' src='./images/back.jpeg' alt='back of card' onClick={handleClick} />
+            </div>
         </div>
     )
 }
