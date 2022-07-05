2e185fbd1e2da69813d4bae56a39c7dc7eb82852
Paige W <ward.paige96@gmail.com>
2022-07-04 21:01:57 -0600

    background video and adjustment of content on top

--- src/App.js
+++ src/App.js
@@ -87,26 +87,33 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Memory: A Catan Twist</h1>
-      <h2>Time Remaining:{" "}
-          {time > 0 ? time : <span>Game over! Better luck next time.</span>}</h2>
-      <h2>Points: {points}</h2>
-      <button onClick={handleClick}>Shuffle</button>
-      <section className="cardLayout">
-        {cards.map((card) => (
-          <Card
-            key={card.id}
-            card={card}
-            handleCard={handleCard}
-            isFlipped={
-              card === firstClick || card === secondClick || card.match === true
-            }
-          />
-        ))}
-      </section>
-      <HighScore trigger={true}>
-        
-      </HighScore>
+
+      <video autoPlay loop muted className="video">
+        <source src="/av/C0089.mp4" type="video/mp4" />
+      </video>
+
+      <div className="content">
+        <audio src="/av/mixkit-forest-near-countryside-farm-1221.wav"></audio>
+        <h1>Memory: A Catan Twist</h1>
+        <h2>Time Remaining:{" "}
+            {time > 0 ? time : <span>Game over! Better luck next time.</span>}</h2>
+        <h2>Points: {points}</h2>
+        <button onClick={handleClick}>Shuffle</button>
+        <section className="cardLayout">
+          {cards.map((card) => (
+            <Card
+              key={card.id}
+              card={card}
+              handleCard={handleCard}
+              isFlipped={
+                card === firstClick || card === secondClick || card.match === true
+              }
+            />
+          ))}
+        </section>
+        <HighScore trigger={true} />
+      </div>
+
     </div>
   );
 }
